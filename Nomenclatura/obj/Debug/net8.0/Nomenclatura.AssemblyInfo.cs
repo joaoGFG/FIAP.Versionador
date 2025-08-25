6d4f65c24f419b67850f5e7209ff18d8660bb6ea
joaoGFG <joaogabrielfg10@gmail.com>
2025-08-25 08:50:25 -0300

    Init

--- Nomenclatura/obj/Debug/net8.0/Nomenclatura.AssemblyInfo.cs
+++ Nomenclatura/obj/Debug/net8.0/Nomenclatura.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Nomenclatura")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+276780f1abef3275d2116583e6aaea6d87085eb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Nomenclatura")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Nomenclatura")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
