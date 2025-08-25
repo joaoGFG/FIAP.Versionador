276780f1abef3275d2116583e6aaea6d87085eb1
joaoGFG <joaogabrielfg10@gmail.com>
2025-08-25 08:41:48 -0300

    Init

---
+++ Nomenclatura/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Nomenclatura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Fiap!");
+        }
+    }
+}
